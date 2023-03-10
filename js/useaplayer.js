4ded4d7eb3f6bde6f619ea5f166e6391f52195e0
CliqueSkywalker <cliqueskywalker@outlook.com>
2023-03-10 22:06:12 +0800

    Site updated: 2023-03-10 22:06:12

--- js/useaplayer.js
+++ js/useaplayer.js
@@ -3,10 +3,11 @@ const ap = new APlayer({
     autoplay: false,
     theme: '#FADFA3',
     loop: 'all',
-    order: 'random',
     preload: 'auto',
     volume: 0.7,
     mutex: true,
+    listFolded: false,
+    listMaxHeight: 90,
     fixed: true,
     audio: [
         {
