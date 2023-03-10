791cad8e1be73dd85bc2b8291a4b93feadddb85e
CliqueSkywalker <cliqueskywalker@outlook.com>
2023-03-10 21:15:12 +0800

    Site updated: 2023-03-10 21:15:12

--- js/useaplayer.js
+++ js/useaplayer.js
@@ -1,7 +1,17 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    fixed: true,
+    mini: false,
+    autoplay: false,
+    theme: '#FADFA3',
+    loop: 'all',
+    order: 'random',
+    preload: 'auto',
+    volume: 0.7,
+    mutex: true,
+    listFolded: false,
+    listMaxHeight: 90,
     lrcType: 3,
+    fixed: true,
     audio: [
         {
             name: 'Street Light',
